af4d0237870a6dd461ed1fcadd4b622cbca3a57f
MaxTravisSmithson <maxtravissmithson@gmail.com>
2025-10-04 23:44:48 +0100

    Episode 17 - Custom Armour Added a custom armor set with all the features of a vanilla set

--- src/main/java/net/max/tutorialmod/datagen/ModModelProvider.java
+++ src/main/java/net/max/tutorialmod/datagen/ModModelProvider.java
@@ -11,6 +11,7 @@ import net.minecraft.data.client.Models;
 import net.minecraft.data.client.TexturedModel;
 import net.minecraft.data.client.TextureMap;
 import net.minecraft.data.client.VariantsBlockStateSupplier;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.util.Identifier;
 
 public class ModModelProvider extends FabricModelProvider {
@@ -62,5 +63,10 @@ public class ModModelProvider extends FabricModelProvider {
         itemModelGenerator.register(ModItems.PINK_GARNET_HOE, Models.HANDHELD);
 
         itemModelGenerator.register(ModItems.PINK_GARNET_HAMMER, Models.HANDHELD);
+
+        itemModelGenerator.registerArmor(((ArmorItem)ModItems.PINK_GARNET_HELMET));
+        itemModelGenerator.registerArmor(((ArmorItem)ModItems.PINK_GARNET_CHESTPLATE));
+        itemModelGenerator.registerArmor(((ArmorItem)ModItems.PINK_GARNET_LEGGINGS));
+        itemModelGenerator.registerArmor(((ArmorItem)ModItems.PINK_GARNET_BOOTS));
     }
 }
